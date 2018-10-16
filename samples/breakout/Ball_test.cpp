55a75e5a4e0af5c7cc9bc163e97e2f01f18f9c6f
Snorri Sturluson <snorri.sturluson@gmail.com>
2018-10-16 22:39:06 +0000

    Restructuring tests to use sections

--- samples/breakout/Ball_test.cpp
+++ samples/breakout/Ball_test.cpp
@@ -7,97 +7,102 @@
 #include "Ball.h"
 #include "Paddle.h"
 
-TEST_CASE("Ball constructor", "[Ball]") {
-    Ball b;
+TEST_CASE("Ball") {
+    SECTION("constructor") {
+        Ball b;
+    }
+    SECTION("SetRadius") {
+        Ball b;
+        b.SetRadius(4.0f);
+        REQUIRE(b.GetRadius() == 4.0f);
+    }
+
+    SECTION("SetPosition") {
+        Ball b;
+
+        b.SetPosition({3.14f, 42.0f});
+
+        glm::vec2 expected {3.14f, 42.0f};
+        REQUIRE(b.GetPosition() == expected);
+    }
+
+    SECTION("CollideWithPaddle") {
+        SECTION("hits") {
+            Ball b;
+            b.SetRadius(2.0f);
+            b.SetPosition({0.0f, 1.0f});
+            b.SetVelocity({0.0f, 1.0f});
+
+            Paddle p;
+            p.SetPosition({0.0f, 0.0f});
+            p.SetWidth(10.0f);
+
+            b.CollideWithPaddle(p);
+
+            REQUIRE(b.GetVelocity().y == -1.0f);
+        }
+        SECTION("misses when ball is to the right") {
+            Ball b;
+            b.SetRadius(2.0f);
+            b.SetPosition({50.0f, 1.0f});
+            b.SetVelocity({0.0f, 1.0f});
+
+            Paddle p;
+            p.SetPosition({0.0f, 0.0f});
+            p.SetWidth(10.0f);
+
+            b.CollideWithPaddle(p);
+
+            REQUIRE(b.GetVelocity().y == 1.0f);
+        }
+
+        SECTION("misses when ball is to the left") {
+            Ball b;
+            b.SetRadius(2.0f);
+            b.SetPosition({-50.0f, 1.0f});
+            b.SetVelocity({0.0f, 1.0f});
+
+            Paddle p;
+            p.SetPosition({0.0f, 0.0f});
+            p.SetWidth(10.0f);
+
+            b.CollideWithPaddle(p);
+
+            REQUIRE(b.GetVelocity().y == 1.0f);
+        }
+
+        SECTION("misses when ball is above") {
+            Ball b;
+            b.SetRadius(2.0f);
+            b.SetPosition({0.0f, -10.0f});
+            b.SetVelocity({0.0f, 1.0f});
+
+            Paddle p;
+            p.SetPosition({0.0f, 0.0f});
+            p.SetWidth(10.0f);
+
+            b.CollideWithPaddle(p);
+
+            REQUIRE(b.GetVelocity().y == 1.0f);
+        }
+
+
+        SECTION("misses when ball is moving away") {
+            Ball b;
+            b.SetRadius(2.0f);
+            b.SetPosition({0.0f, 1.0f});
+            b.SetVelocity({0.0f, -1.0f});
+
+            Paddle p;
+            p.SetPosition({0.0f, 0.0f});
+            p.SetWidth(10.0f);
+
+            b.CollideWithPaddle(p);
+
+            REQUIRE(b.GetVelocity().y == -1.0f);
+        }
+    }
+
 }
 
-TEST_CASE("Ball::SetRadius", "[Ball]") {
-    Ball b;
-    b.SetRadius(4.0f);
-    REQUIRE(b.GetRadius() == 4.0f);
-}
 
-TEST_CASE("Ball::SetPosition", "[Ball]") {
-    Ball b;
-
-    b.SetPosition({3.14f, 42.0f});
-
-    glm::vec2 expected {3.14f, 42.0f};
-    REQUIRE(b.GetPosition() == expected);
-}
-
-TEST_CASE("Ball::CollideWithPaddle hits", "[Ball]") {
-    Ball b;
-    b.SetRadius(2.0f);
-    b.SetPosition({0.0f, 1.0f});
-    b.SetVelocity({0.0f, 1.0f});
-
-    Paddle p;
-    p.SetPosition({0.0f, 0.0f});
-    p.SetWidth(10.0f);
-
-    b.CollideWithPaddle(p);
-
-    REQUIRE(b.GetVelocity().y == -1.0f);
-}
-
-TEST_CASE("Ball::CollideWithPaddle misses when ball is to the right", "[Ball]") {
-    Ball b;
-    b.SetRadius(2.0f);
-    b.SetPosition({50.0f, 1.0f});
-    b.SetVelocity({0.0f, 1.0f});
-
-    Paddle p;
-    p.SetPosition({0.0f, 0.0f});
-    p.SetWidth(10.0f);
-
-    b.CollideWithPaddle(p);
-
-    REQUIRE(b.GetVelocity().y == 1.0f);
-}
-
-TEST_CASE("Ball::CollideWithPaddle misses when ball is to the left", "[Ball]") {
-    Ball b;
-    b.SetRadius(2.0f);
-    b.SetPosition({-50.0f, 1.0f});
-    b.SetVelocity({0.0f, 1.0f});
-
-    Paddle p;
-    p.SetPosition({0.0f, 0.0f});
-    p.SetWidth(10.0f);
-
-    b.CollideWithPaddle(p);
-
-    REQUIRE(b.GetVelocity().y == 1.0f);
-}
-
-TEST_CASE("Ball::CollideWithPaddle misses when ball is above", "[Ball]") {
-    Ball b;
-    b.SetRadius(2.0f);
-    b.SetPosition({0.0f, -10.0f});
-    b.SetVelocity({0.0f, 1.0f});
-
-    Paddle p;
-    p.SetPosition({0.0f, 0.0f});
-    p.SetWidth(10.0f);
-
-    b.CollideWithPaddle(p);
-
-    REQUIRE(b.GetVelocity().y == 1.0f);
-}
-
-
-TEST_CASE("Ball::CollideWithPaddle misses when ball is moving away", "[Ball]") {
-    Ball b;
-    b.SetRadius(2.0f);
-    b.SetPosition({0.0f, 1.0f});
-    b.SetVelocity({0.0f, -1.0f});
-
-    Paddle p;
-    p.SetPosition({0.0f, 0.0f});
-    p.SetWidth(10.0f);
-
-    b.CollideWithPaddle(p);
-
-    REQUIRE(b.GetVelocity().y == -1.0f);
-}
--- samples/breakout/Paddle_test.cpp
+++ samples/breakout/Paddle_test.cpp
@@ -5,65 +5,69 @@
 #include "catch.hpp"
 #include "Paddle.h"
 
-TEST_CASE("Paddle constructor", "[Paddle]") {
-    Paddle p;
+
+TEST_CASE("Paddle") {
+    SECTION("constructor") {
+        Paddle p;
+    }
+
+    SECTION("newly created has zero velocity") {
+        Paddle p;
+        REQUIRE(p.GetVelocity().x == 0.0f);
+        REQUIRE(p.GetVelocity().y == 0.0f);
+    }
+
+    SECTION("SetPosition") {
+        Paddle p;
+        p.SetPosition({3.14f, 42.0f});
+
+        glm::vec2 expected {3.14f, 42.0f};
+        REQUIRE(p.GetPosition() == expected);
+    }
+
+    SECTION("SetWidth") {
+        Paddle p;
+        p.SetWidth(10.0f);
+
+        REQUIRE(p.GetWidth() == 10.0f);
+    }
+
+    SECTION("Update") {
+        Paddle p;
+        p.SetPosition({0, 0});
+        p.SetVelocity({10, 0});
+        p.Update(1);
+
+        glm::vec2 expected {10, 0};
+        REQUIRE(p.GetPosition().x == expected.x);
+        REQUIRE(p.GetPosition().y == expected.y);
+    }
+
+    SECTION("SetBounds") {
+        SECTION("left") {
+            Paddle p;
+            p.SetWidth(10.0f);
+            p.SetBounds(0, 1000);
+            p.SetPosition({0, 0});
+            p.SetVelocity({-10, 0});
+            p.Update(1);
+
+            glm::vec2 expected {5.0f, 0};
+            REQUIRE(p.GetPosition().x == expected.x);
+            REQUIRE(p.GetPosition().y == expected.y);
+        }
+
+        SECTION("right") {
+            Paddle p;
+            p.SetWidth(10.0f);
+            p.SetBounds(0, 1000);
+            p.SetPosition({990, 0});
+            p.SetVelocity({10, 0});
+            p.Update(1);
+
+            glm::vec2 expected {995.0f, 0};
+            REQUIRE(p.GetPosition().x == expected.x);
+            REQUIRE(p.GetPosition().y == expected.y);
+        }
+    }
 }
-
-TEST_CASE("Paddle Newly created has zero velocity", "[Paddle]") {
-    Paddle p;
-    REQUIRE(p.GetVelocity().x == 0.0f);
-    REQUIRE(p.GetVelocity().y == 0.0f);
-}
-
-TEST_CASE("Paddle::SetPosition", "[Paddle]") {
-    Paddle p;
-    p.SetPosition({3.14f, 42.0f});
-
-    glm::vec2 expected {3.14f, 42.0f};
-    REQUIRE(p.GetPosition() == expected);
-}
-
-TEST_CASE("Paddle::SetWidth", "[Paddle]") {
-    Paddle p;
-    p.SetWidth(10.0f);
-
-    REQUIRE(p.GetWidth() == 10.0f);
-}
-
-TEST_CASE("Paddle::Update", "[Paddle]") {
-    Paddle p;
-    p.SetPosition({0, 0});
-    p.SetVelocity({10, 0});
-    p.Update(1);
-
-    glm::vec2 expected {10, 0};
-    REQUIRE(p.GetPosition().x == expected.x);
-    REQUIRE(p.GetPosition().y == expected.y);
-}
-
-TEST_CASE("Paddle::SetBounds Left", "[Paddle]") {
-    Paddle p;
-    p.SetWidth(10.0f);
-    p.SetBounds(0, 1000);
-    p.SetPosition({0, 0});
-    p.SetVelocity({-10, 0});
-    p.Update(1);
-
-    glm::vec2 expected {5.0f, 0};
-    REQUIRE(p.GetPosition().x == expected.x);
-    REQUIRE(p.GetPosition().y == expected.y);
-}
-
-TEST_CASE("Paddle::SetBounds Right", "[Paddle]") {
-    Paddle p;
-    p.SetWidth(10.0f);
-    p.SetBounds(0, 1000);
-    p.SetPosition({990, 0});
-    p.SetVelocity({10, 0});
-    p.Update(1);
-
-    glm::vec2 expected {995.0f, 0};
-    REQUIRE(p.GetPosition().x == expected.x);
-    REQUIRE(p.GetPosition().y == expected.y);
-}
-
