8fd8766357e41bdb90c45dd241b2465fbcc6aa23
Snorri Sturluson <snorri.sturluson@gmail.com>
2018-10-30 08:16:20 +0000

    Tweaking particles sample

--- samples/particles/ParticlesApp.cpp
+++ samples/particles/ParticlesApp.cpp
@@ -38,15 +38,20 @@ void ParticlesApp::Run() {
     std::default_random_engine random_engine;
     std::uniform_real_distribution<float> rng;
 
-    for(int i = 0; i < 5000; ++i) {
+    std::vector<std::shared_ptr<ParticleEmitter>> emitters;
+
+    for(int i = 0; i < 1000; ++i) {
         auto em = ps.AddEmitter();
-        em->SetEmissionRate(rng(random_engine) * 50.0f + 10.0f);
+        em->SetEmissionRate(rng(random_engine) * 500.0f + 10.0f);
         em->SetPosition({rng(random_engine) * 800, rng(random_engine) * 600});
-        em->SetLifespan(rng(random_engine) * 5.0f + 1.0f);
+        em->SetLifespan(rng(random_engine) * 1.0f + 0.5f);
         em->SetDirection(rng(random_engine) * M_PI * 2.0f);
         em->SetDirectionRange(0.1f);
-        em->SetSpeed(rng(random_engine) * 10.0f + 10.0f);
+        em->SetSpeed(rng(random_engine) * 100.0f + 10.0f);
+        emitters.push_back(em);
     }
+
+    auto emIt = emitters.begin();
 
     r.SetClearColor({0.0f, 0.0f, 0.0f, 1.0f});
     while (!glfwWindowShouldClose(m_window)) {
@@ -54,6 +59,13 @@ void ParticlesApp::Run() {
 
         glfwPollEvents();
 
+        if(rng(random_engine) < 0.4f) {
+            (*emIt)->SetDirection(rng(random_engine) * M_PI * 2.0f);
+            emIt++;
+            if(emIt == emitters.end()) {
+                emIt = emitters.begin();
+            }
+        }
         float td = 1.0f / 60.0f;
         ps.Update(td);
 
